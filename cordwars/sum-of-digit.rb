bae89153def4f297410eb04a298bb69b06ad7bc3
rootsuke <rootsuke@gmail.com>
2019-08-10 04:20:09 +0900

    refactor

--- cordwars/sum-of-digit.rb
+++ cordwars/sum-of-digit.rb
@@ -15,12 +15,28 @@ def digital_root_a(n)
 end
 
 def digital_root(n)
-  if n % 9 == 0
-    n == 0 ? n : 9
-  else
-    n % 9
-  end
+  # &&は左辺がfalseのときは右辺は評価せず、左辺がtrueのときのみ右辺を評価する
+  # つまり「!(n % 9).zero?」がfalseであれば、その時点で「n % 9」は評価されず、最後に評価された「!(n % 9).zero?」の値(false)を返す
+  #   => 「|| n.zero? && n || 9 」以降の評価に移る
+  # 逆に「!(n % 9).zero?」がtrueなら「n % 9」が評価され、その値(nを9で割った余り)を返す。
+  
+  # ||は左辺が真のときは右辺は評価しない
+  # 「!(n % 9).zero? && n % 9」が真なら、その時点で「n.zero? && n || 9」は無視される
+  #   => 最後に評価された「n % 9」の値が返る
+
+  !(n % 9).zero? && n % 9 || n.zero? && n || 9
+
+  # if n % 9 == 0
+  #   if n == 0
+  #     n
+  #   else
+  #     9
+  #   end
+  # else
+  #   n % 9
+  # end
+
   # (n % 9).zero? ? n.zero? ? n : 9 : n % 9
 end
 
-p digital_root(132189)
+p digital_root(0)
